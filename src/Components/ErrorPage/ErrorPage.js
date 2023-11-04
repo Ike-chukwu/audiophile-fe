4f30ccd2d6128489d791b907da341512d79c4b13
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2023-11-04 01:58:31 +0100

    fix:errorPage, cnacelPage, checkoupage all responsive

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./ErrorPage.css";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="error-container">
       <div className="error-body">
         Sorry! An error has occured. <br /> Please try again!
       </div>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+        className="error-btn"
+      >
+        Back to home
+      </button>
     </div>
   );
 };
